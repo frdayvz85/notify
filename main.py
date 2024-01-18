61ed0cc27604a582066185e4807981e002fa9641
Farid Eyvazov <ferideyvazov25@gmail.com>
2024-01-18 23:25:17 +0100

    Notifications added

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from typing import Optional
-
-from fastapi import FastAPI
-
+# status_handler.py for CONNECTIVITY
+ 
+# run with: uvicorn status_handler:app
+ 
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+ 
+from typing_extensions import Annotated
+from typing import Union
+ 
 app = FastAPI()
+ 
+class ConnectivityEventDetail(BaseModel):
+    deviceStatus: str
+ 
+class Event(BaseModel):
+    eventType: str
+    eventTime: str
+    eventDetail: ConnectivityEventDetail
+ 
+class Notification(BaseModel):
+    eventSubscriptionId: str
+    event: Event
+ 
+
+@app.get("/test")
+def test():
+   return {"message": "Hello, world"}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/notifications")
+def receive_notification(notification: Notification,
+                         authorization: Annotated[Union[str, None], Header]):
+    if authorization == "Bearer nacauth":
+      if notification.event.eventDetail.deviceStatus == "REACHABLE":
+        print("Device is available")
+      elif notification.event.eventDetail.deviceStatus == "UNREACHABLE":
+        print("Device is not available")
